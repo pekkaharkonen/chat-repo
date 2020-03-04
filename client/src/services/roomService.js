2a264c647babaeecfa6cc75e417fe79771e592aa
Marjut <marjut.kinnari@gmail.com>
2020-03-04 10:55:00 +0200

    Roomlist, service folder with roomService edited

---
+++ client/src/services/roomService.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';¨
+import axios from 'axios';
+const apiUrl="/api/rooms";
+
+async function getAllRooms(){
+    let res = await axios.get(apiUrl)
+    return res.data;
+}
+
+export {getAllRooms};
