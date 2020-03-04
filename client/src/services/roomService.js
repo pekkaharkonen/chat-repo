542a45cd34e8b1176a852d9688c05883f6154664
Pekka Härkönen <pekka.o.harkonen@utu.fi>
2020-03-04 13:40:22 +0200

    merge conficts solved

--- client/src/services/roomService.js
+++ client/src/services/roomService.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
 import axios from 'axios';
-const apiUrl="/api/rooms";
+const apiUrl = '/api/rooms';
 
-async function getAllRooms(){
-    let res = await axios.get(apiUrl)
-    return res.data;
+async function getAllRooms() {
+  let res = await axios.get(apiUrl);
+  return res.data;
 }
 
-export {getAllRooms};
+export { getAllRooms };
