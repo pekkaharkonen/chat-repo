2c63d95797a4cd87b232f4e033bdca479a99e7f3
Otto Eklund <ottoeklund@gmail.com>
2020-03-05 15:08:41 +0200

    addition to previous commit. sockets work.

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,11 @@ import Login from './components/pages/landing/Login';
 import Register from './components/pages/landing/Register';
 import Rooms from './components/pages/rooms/Rooms';
 import ProtectedRoute from './components/ProtectedRoute';
-import SocketTest from './components/socketTest';
 
 
 function App() {
   return (
     <div className='App'>
-      <SocketTest />
       <Router>
         <Switch>
           <Route exact path='/' component={Landing} />
