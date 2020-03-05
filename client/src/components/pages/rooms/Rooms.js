de9d85c6ea061d51864a33a009d9414a0703d05f
Pekka Härkönen <pekka.o.harkonen@utu.fi>
2020-03-05 16:18:03 +0200

    logout button implemented

--- client/src/components/pages/rooms/Rooms.js
+++ client/src/components/pages/rooms/Rooms.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Roomlist from './RoomList';
 import { getAllRooms, postRooms } from '../../../services/roomService';
 import CreateRoom from './CreateRoom';
+import { Button } from '@material-ui/core';
 
 class Rooms extends Component {
   state = { rooms: [] };
@@ -9,14 +10,20 @@ class Rooms extends Component {
     let rooms = await getAllRooms(); // Kesken, vaatii toteutusta
     this.setState({ rooms });
   };
-  createRoom = async(newroom) => {
-      let response = await postRooms(newroom);
-      this.setState(this.state);
-      this.componentDidMount();
-  }
+  createRoom = async newroom => {
+    let response = await postRooms(newroom);
+    this.setState(this.state);
+    this.componentDidMount();
+  };
+
+  handleLogout = () => {
+    localStorage.removeItem('token');
+    this.props.history.push('/');
+  };
   render() {
     return (
       <div>
+        <Button onClick={this.handleLogout}>Log out</Button>
         <CreateRoom isReady={this.createRoom} />
         <Roomlist rooms={this.state.rooms} />
       </div>
