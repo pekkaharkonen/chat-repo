c46476dd57f381f9e0b95eb139c7b1b23201eef7
Otto Eklund <ottoeklund@gmail.com>
2020-03-06 11:44:33 +0200

    redirect all http requests to one port

--- client/src/services/clientSocketService.js
+++ client/src/services/clientSocketService.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 import io from "socket.io-client"
 
 let socket
 
 export const socketConnection = () => {
-    socket = io(process.env.SOCKET_IO_SERVER)
+    socket = io("http://localhost:8000")
         .on("greeting", (msg) => {
             console.log(msg)
             socket.emit("echo", "well hello")
@@ -55,62 +54,4 @@ export const sendMessage = (msg) => {
 //         console.log('received socket error:')
 //         console.log(err)
 //     })
-=======
-// import io from "socket.io-client"
-
-// let socket
-
-// export const socketConnection = () => {
-//     socket = io("http://localhost:8000")
-//         .on("greeting", (msg) => {
-//             console.log(msg)
-//             socket.emit("echo", "well hello")
-//         })
-//         .on("availableUsers", msg => {
-//             console.log(msg)
-//         })
-//         .on("new message", (msg) => {
-//             console.log(msg)
-//         })
->>>>>>> 79d7b6295ceac0b519f5cd015242f8c5947480cd
 // }
-
-// //socket functions
-// export const login = (username) => {
-//     socket.emit("login", username)
-// }
-// export const getAvailableUsers = () => {
-//     socket.emit('getAvailableUsers')
-// }
-// export const disconnect = () => {
-//     socket.off("disconnect")
-// }
-// export const sendMessage = (msg) => {
-//     console.log(msg + " from client")
-//     console.log(socket)
-//     socket.emit("message", "msg")
-// }
-
-
-// //     function join(chatroomName, cb) {
-// //         socket.emit('join', chatroomName, cb)
-// //     }
-
-// //     function leave(chatroomName, cb) {
-// //         socket.emit('leave', chatroomName, cb)
-// //     }
-
-// //     function message(chatroomName, msg, cb) {
-// //         socket.emit('message', { chatroomName, message: msg }, cb)
-// //     }
-
-// //     function getChatrooms(cb) {
-// //         socket.emit('chatrooms', null, cb)
-// //     }
-
-
-// //     socket.on('error', function (err) {
-// //         console.log('received socket error:')
-// //         console.log(err)
-// //     })
-// // }
